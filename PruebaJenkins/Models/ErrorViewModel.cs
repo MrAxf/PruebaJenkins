beee1df8be0d5629084b6f4ef0c871714c0a9afa
Axford <thaxford@gmail.com>
2018-05-23 18:49:44 +0200

    Agregar archivos de proyecto.

---
+++ PruebaJenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaJenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
